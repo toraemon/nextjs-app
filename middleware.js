4172e7be159952affe5983c799e6e80ca49b3263
toraemon <nyanpeko@gmail.com>
2024-12-09 09:49:20 +0900

    chapter6

--- middleware.js
+++ middleware.js
@@ -1,16 +1,19 @@
+import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-    const token = await request.headers.get("Authorization")?.split(" ")[1]
+    const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiZXhwIjoxNzMzNzkwMjYzfQ.x4pvfiC2Hw6lhtCm6WMvDk4X7t-xKUM7dxVAOsdZdCg"
+    // const token = await request.headers.get("Authorization")?.split(" ")[1]
     if (!token) {
         return NextResponse.json({ message: "トークンがありません" })
     }
 
     try {
         const secretKey = new TextEncoder().encode("next-market-app-book")
-        const decodedJwt = await jwtVerify(token, secretKey)
+        await jwtVerify(token, secretKey)
         return NextResponse.next()
-    } catch {
+    } catch (error) {
+        console.log('error: ', error);
         return NextResponse.json({ message: "トークンが正しくないので、ログインしてください" })
     }
 }
